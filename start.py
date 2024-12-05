5a1e2cefc6c262a0a06ca0d8bd4272013205228a
tri <huutri231103@gmail.com>
2024-12-05 15:37:37 +0700

    fix

--- start.py
+++ start.py
@@ -1,5 +1,45 @@
-from api.main import Host_IP, Host_Port
-import uvicorn
+from crawlers.tiktok.web_crawler import TikTokWebCrawler
+import pandas as pd
+import asyncio
 
-if __name__ == '__main__':
-    uvicorn.run('api.main:app', host=Host_IP, port=Host_Port, reload=True, log_level="info")
+async def main():
+    tiktok_crawler = TikTokWebCrawler()
+
+    data = pd.read_csv('user_id.csv')
+    print(data['unique_id'][0])
+    print(len(data))
+
+    user_data = []
+
+    for i in range(len(data)):
+        uniqueId = data['unique_id'][i]
+        secUid = ""  
+
+        try:
+            user_profile = await tiktok_crawler.get_user_profile(secUid, uniqueId)
+            # print(user_profile.get('userInfo'))
+            # break
+            user_info = user_profile.get('userInfo')
+            # print(user_info)
+            stats = user_info.get('stats')
+            user = user_info.get('user')
+
+            extracted_data = {
+                "uid": user.get('uniqueId'),
+                "name": user.get('nickname'),
+                "followers": stats.get('followerCount'),
+                "likes": stats.get('heartCount'),
+                "bio": user.get('signature'),
+            }
+
+            # print(extracted_data)
+            user_data.append(extracted_data)
+
+        except Exception as e:
+            print(f"Could not get user with uniqueId: {uniqueId}. Error: {e}")
+
+    if user_data:
+        pd.DataFrame(user_data).to_csv("user_data.csv", index=False)
+        print("Dữ liệu đã được lưu vào user_data.csv")
+
+asyncio.run(main())
